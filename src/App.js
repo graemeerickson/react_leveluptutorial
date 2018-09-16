6475db36ef1d6302da21c7365470139445d14d47
graemeerickson <graemeerickson@gmail.com>
2018-09-16 14:29:53 -0700

    Implemented props

--- src/App.js
+++ src/App.js
@@ -8,13 +8,22 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Welcome text="Welcome to React" />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
+  }
+}
+
+class Welcome extends Component {
+  render() {
+    const { text } = this.props;
+    return (
+      <h1 className="App-title">{text}</h1>
+    )
   }
 }
 

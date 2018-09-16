a1d3d83ad8bbe85a21f0668ff490a4f28219312a
graemeerickson <graemeerickson@gmail.com>
2018-09-16 15:50:14 -0700

    Added movies array and implemented loop to render new component per movie

--- src/App.js
+++ src/App.js
@@ -2,46 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './Movie';
+
+const movies = [{
+    id: 1,
+    title: 'Star Wars'
+  }, {
+    id: 2,
+    title: 'Spiderman'
+  }, {
+    id: 3,
+    title: 'That Thing You Do!'
+  },
+];
+
 class App extends Component {
-
-  state = {
-    input: 'Hello'
-  }
-
-  updateInput = (event) => {
-    this.setState({
-      input: event.target.value.trim()
-    })
-  }
-
-  submit = () => {
-    console.log(this.text.value);
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome to React" />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h3>{this.state.input}</h3>
-        <input type="text" onChange={this.updateInput} value={this.state.input} />
-        <button onClick={this.submit}>Show value</button>
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
-  }
-}
-
-class Welcome extends Component {
-  render() {
-    const { text } = this.props;
-    return (
-      <h1 className="App-title">{text}</h1>
-    )
   }
 }
 

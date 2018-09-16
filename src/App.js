74843c8a31223ef3948a0d762cba873f55441e89
graemeerickson <graemeerickson@gmail.com>
2018-09-16 15:03:13 -0700

    Implemented controlled input using state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import './App.css';
 
 class App extends Component {
 
+  state = {
+    input: 'Hello'
+  }
+
+  updateInput = (event) => {
+    this.setState({
+      input: event.target.value.trim()
+    })
+  }
+
   submit = () => {
     console.log(this.text.value);
   }
@@ -18,7 +28,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <input type="text" ref={(input) => this.text = input } />
+        <h3>{this.state.input}</h3>
+        <input type="text" onChange={this.updateInput} value={this.state.input} />
         <button onClick={this.submit}>Show value</button>
       </div>
     );
